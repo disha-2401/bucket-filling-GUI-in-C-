2b3d3e4ac22ced7393a0d981a1858e0e5b63943f
disha-2401 <dishamiyani03@gmail.com>
2021-04-30 16:03:16 -1200

    added GUI project

---
+++ bucket-filling-GUI-in-CSharp/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Lab5a
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        // <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ColorButton = new System.Windows.Forms.Button();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.PictureBox = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ColorButton
+            // 
+            this.ColorButton.Location = new System.Drawing.Point(47, 17);
+            this.ColorButton.Margin = new System.Windows.Forms.Padding(5);
+            this.ColorButton.Name = "ColorButton";
+            this.ColorButton.Size = new System.Drawing.Size(117, 32);
+            this.ColorButton.TabIndex = 0;
+            this.ColorButton.Text = "Color";
+            this.ColorButton.UseVisualStyleBackColor = true;
+            this.ColorButton.Click += new System.EventHandler(this.colorButon_click);
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Location = new System.Drawing.Point(250, 17);
+            this.CloseButton.Margin = new System.Windows.Forms.Padding(5);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(117, 32);
+            this.CloseButton.TabIndex = 1;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.close_Program);
+            // 
+            // PictureBox
+            // 
+            this.PictureBox.Image = ((System.Drawing.Image)(resources.GetObject("PictureBox.Image")));
+            this.PictureBox.Location = new System.Drawing.Point(40, 200);
+            this.PictureBox.Margin = new System.Windows.Forms.Padding(5);
+            this.PictureBox.Name = "PictureBox";
+            this.PictureBox.Size = new System.Drawing.Size(99, 69);
+            this.PictureBox.TabIndex = 3;
+            this.PictureBox.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(113, 98);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(5);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.trackBar1.Size = new System.Drawing.Size(184, 45);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.Scroll += new System.EventHandler(this.TrackBarScroll);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.TimerTick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(110, 78);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Low";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(264, 78);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "High";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(402, 500);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.PictureBox);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.ColorButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.paintForm);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ColorButton;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.PictureBox PictureBox;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ bucket-filling-GUI-in-CSharp/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5a
+{
+   
+    public partial class Form1 : Form
+    {
+        private Color color; //store brush color 
+        private int counter; //tracking variable that indicates how much water should be poured in bucket
+        private bool stopped; //indicates if Faucet is stopped 
+        private bool filled; //indicates if bucket is filled
+        int[] speedTracker = { 0,180, 160, 140, 120, 100, 80, 60, 40, 20, 10 }; // array containing water falling speed for different levels on trackbar
+        private Graphics graphics;
+
+        public Form1()
+        {
+            // color stored as light blue by default
+            color = Color.LightBlue;
+
+            // counter set to 1 as the bucket doesn't contain water
+            counter = 1;
+
+            //initially faucet is stopped
+            stopped = true;
+
+            //bucket is not filled
+            filled = false; 
+
+            InitializeComponent();
+
+            // Keeps declaring Paint object and passes "paintingForm" as method argument
+            Paint += new PaintEventHandler(paintForm);
+
+            // set to 0 initially
+            trackBar1.Value = 0;
+        }
+        
+        // Method that invoked by Paint Class which draws lines of the bucket
+        private void paintForm(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawLine(new Pen(Color.White), 100, 290, 100, 400);
+            e.Graphics.DrawLine(new Pen(Color.White), 100, 400, 300, 400);
+            e.Graphics.DrawLine(new Pen(Color.White), 300, 400, 300, 290);
+        }
+
+        //show color dialog when color button is clicked
+        private void colorButon_click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                color = colorDialog1.Color;
+        }
+        
+        //close program when close button is clicked
+        private void close_Program(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        
+        // method invoked evrytime the trackbar is scrolled in the program
+        private void TrackBarScroll(object sender, EventArgs e)
+        {
+            // create Graphics object
+            graphics = CreateGraphics();
+
+            // if bucket if filled then clears bucket then sets bool filled to false
+            if (filled)
+            {
+                graphics.FillRectangle(new System.Drawing.SolidBrush(BackColor), 101, 200, 199, 200);
+                filled = false;
+            }
+
+            // start timer and set stopped to false if Faucet is stopped
+            if (stopped)
+            {
+                timer1.Start();
+                stopped = false;
+            }
+
+            // setting interval from the speedTrackar array using trackbar value as index if the trackbar value is greater than 0 
+            if (trackBar1.Value > 0)
+            {
+                timer1.Interval = speedTracker[trackBar1.Value];
+            }
+
+            // stop Faucet if trackbar value is 0
+            if (trackBar1.Value == 0)
+            {
+                timer1.Stop();
+                stopped = true;
+                graphics.FillRectangle(new SolidBrush(BackColor), 110, 200, 15, 200 - counter + 1);
+            }
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            graphics = CreateGraphics();
+
+            // genreate rectangle using FillRectangle using brush object as first argument, and coordinates as second argument
+            graphics.FillRectangle(new SolidBrush(color), 101, 400 - counter, 199, 1);
+            graphics.FillRectangle(new SolidBrush(color), 110, 200, 15, 200 - counter + 1);
+            counter += 1;
+
+            // if bucket is filled
+            if ((400 - counter) == 295)
+            {
+                //set filled and stopped to true since bucket is filled
+                filled = true;
+                stopped = true;
+
+                //stop timer
+                timer1.Stop();
+
+                //no need to fill water anymore so set counter to 0
+                counter = 0;
+
+                //set trackbar to initial value
+                trackBar1.Value = 0;
+
+                // create black rectangle to make future rectangle visible
+                graphics.FillRectangle(new SolidBrush(BackColor), 110, 200, 15, 96);
+            }
+        }
+    }
+}
+
